dbbae575d9a9c91030c4dbd2d8a867b0f8695bb9
vinay <vinay.dpeocit@gmail.com>
2019-04-30 09:14:39 +0530

    hello

--- GitHub/Controllers/HomeController.cs
+++ GitHub/Controllers/HomeController.cs
@@ -20,11 +20,6 @@ namespace GitHub.Controllers
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+       
     }
 }
