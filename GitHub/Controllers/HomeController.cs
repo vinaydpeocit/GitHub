dffc0b170b71fec6e3a3e9af5c8abf029963675b
vinay <vinay.dpeocit@gmail.com>
2019-05-06 11:00:29 +0530

    nages in controller

--- GitHub/Controllers/HomeController.cs
+++ GitHub/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace GitHub.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page";
 
             return View();
         }
